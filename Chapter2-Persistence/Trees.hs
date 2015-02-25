a1bec820d6af38675ad8aea733f083ff78b38b1f
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-02-24 23:16:56 -0500

    Implemented exercises 2.3 and 2.4, I think. But 2.5 part a is wrong, I think.

--- Chapter2-Persistence/Trees.hs
+++ Chapter2-Persistence/Trees.hs
@@ -53,17 +53,38 @@ memberShort x tree = go x tree Nothing 0
     go x (Node y left right) cand comps = if (x <= y) then (go x left (Just y) (comps+1)) 
                                           else (go x right cand (comps+1))
 
+-- Exercise 2.3 Inserting an existing element into a binary search tree copies the entire search
+-- path plan even though the copied nodes are indistinguisable from the originals. Rewrite insert
+-- using Exceptions to avoid this copying. Establish only one handler per insertion rather than one handler
+-- per iteration.
+insertExc :: (Ord a) => a -> Tree a -> Tree a
+insertExc x Empty               = Node x Empty Empty
+insertExc x (Node y left right) = case x `compare` y of
+                                   GT -> insertExc x right
+                                   LT -> insertExc x left
+                                   EQ -> error "already present in tree!"
+
+-- Exercise 2.4 Combine the ideas of the previous two exercises to obtain a version of 
+-- insert that performs no unnecessary copying and uses no more than `d+1` comparisons
+insertShort :: (Ord a) => a -> Tree a -> Tree a
+insertShort x tree = go x tree Nothing
+  where
+    go x Empty cand               = case cand of Nothing  -> (Node x Empty Empty)
+                                                 (Just y) -> if (x == y) then (error "!") else (Node x Empty Empty)
+    go x (Node y left right) cand = if (x <= y) then (Node y (go x left (Just y)) right)
+                                                else (Node y left (go x right cand))
+
 -- Exercise 2.5
 -- Part a: make a `complete a Int` function that creates a tree of 
 -- depth Int, putting a in every leaf of the tree.
 -- TODO: does it run in O(d) time? 
---complete :: a -> Integer -> Maybe (Tree a)
---complete x depth 
--- | depth < 0  = Nothing
--- | otherwise  = Just $ complete' depth
---                        where complete' d 
---                                | d == 0    = Empty
---                                | otherwise = let copiedTree = complete' (d-1) 
---                                              in Node x copiedTree copied
+complete :: a -> Integer -> Maybe (Tree a)
+complete x depth 
+ | depth < 0  = Nothing
+ | otherwise  = Just $ complete' depth
+                        where complete' d 
+                                | d == 0    = Empty
+                                | otherwise = let copiedTree = complete' (d-1) 
+                                              in Node x copiedTree copiedTree
 
 -- Part B: Re-visit once discuss `complete`
